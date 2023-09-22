46d7c17d9e58b293bca86a11e42b7411b5447214
eazaykova <lena.zaykova.95@gmail.com>
2023-09-22 22:51:30 +0700

    Added data - options

---
+++ src/data/options.data.ts
@@ -0,0 +1,12 @@
+import { IOption } from "../models/IOption";
+
+export const options: IOption[] = [
+  {
+    value: "brand",
+    name: "По бренду",
+  },
+  {
+    value: "category",
+    name: "По категории",
+  },
+];
