d339d1d85690e0fb40ccf0ad26bb5ad9bb027d65
eazaykova <lena.zaykova.95@gmail.com>
2023-09-22 18:52:03 +0700

    Added ui component - input

---
+++ src/components/ui/input/Input.tsx
@@ -0,0 +1,14 @@
+import styles from "./Input.module.scss";
+
+interface InputProps {
+  type: string;
+  placeholder: string;
+}
+
+const Input = ({ type, placeholder }: InputProps) => {
+  return (
+    <input className={styles.input} type={type} placeholder={placeholder} />
+  );
+};
+
+export default Input;
