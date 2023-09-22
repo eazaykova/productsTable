1cfadc69f898033ac90f939972553874eb04bd44
eazaykova <lena.zaykova.95@gmail.com>
2023-09-22 23:02:01 +0700

    Interface added

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,15 +1,23 @@
-import Button from "../ui/button/Button";
 import Input from "../ui/input/Input";
 import Label from "../ui/label/Label";
 import styles from "./Search.module.scss";
 
-const Search = () => {
+interface SearchProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search = ({ value, onChange }: SearchProps) => {
   return (
     <div className={styles.search}>
       <Label caption="Поиск по названию:">
-        <Input type="search" placeholder="Введите название" />
+        <Input
+          type="search"
+          placeholder="Введите название"
+          value={value}
+          onChange={onChange}
+        />
       </Label>
-      <Button>Найти</Button>
     </div>
   );
 };
