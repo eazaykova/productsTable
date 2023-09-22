64fe835c42c121dce6f889848a9fcd6cb31cde00
eazaykova <lena.zaykova.95@gmail.com>
2023-09-22 18:47:25 +0700

    Added IProduct.ts interface

---
+++ src/models/IProduct.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  images: string[];
+}
