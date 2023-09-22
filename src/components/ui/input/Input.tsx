556289dbba473909e379500494de467ab70548c3
eazaykova <lena.zaykova.95@gmail.com>
2023-09-22 22:59:02 +0700

    Added props: value and onChange

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -3,11 +3,19 @@ import styles from "./Input.module.scss";
 interface InputProps {
   type: string;
   placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ type, placeholder }: InputProps) => {
+const Input = ({ type, placeholder, value, onChange }: InputProps) => {
   return (
-    <input className={styles.input} type={type} placeholder={placeholder} />
+    <input
+      className={styles.input}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
   );
 };
 
