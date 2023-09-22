0aeba6386bcfedc5ca0d1100f8edb8d5d4c47943
eazaykova <lena.zaykova.95@gmail.com>
2023-09-22 18:54:07 +0700

    Added ui component - table

---
+++ src/components/ui/table/Table.tsx
@@ -0,0 +1,29 @@
+import { cellTitle } from "../../../data/cellTitle.data";
+import { IProduct } from "../../../models/IProduct";
+import styles from "./Table.module.scss";
+import TableItem from "./tableItem/TableItem";
+
+interface TableProps {
+  products: IProduct[];
+}
+
+const Table = ({ products }: TableProps) => {
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          {cellTitle.map((title) => (
+            <th key={`__key_${title}`}>{title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <TableItem product={product} key={product.id} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
