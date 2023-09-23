4a7f531722b65668e3bb81726e0c2bb1b861d2f8
eazaykova <lena.zaykova.95@gmail.com>
2023-09-23 11:49:31 +0700

    Refinement of the request and typing

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,12 +1,18 @@
 import axios from "axios";
+import { IData } from "../models/IData";
 
 export default class ProductsService {
   static async getAll(limit: number, page: number) {
-    const response = await axios.get(
-      `https://dummyjson.com/products?limit=${limit}&skip=${
-        (page - 1) * limit
-      }&select=id,title,description,price,rating,stock,brand,category,images`
-    );
+    const response = await axios<IData>({
+      url: "https://dummyjson.com/products",
+      method: "GET",
+      params: {
+        limit: limit,
+        skip: (page - 1) * limit,
+        select: "id,title,description,price,rating,stock,brand,category,images",
+      },
+    });
+
     return response;
   }
 }
